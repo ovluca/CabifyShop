148afe79e1b348b8b09c00c01c5097b6df1f81f5
Ovidiu Luca <ov.luca@gmail.com>
2020-09-10 18:55:41 +0300

    Adding navigation graph component

--- app/src/main/java/com/example/cabifyshop/ui/main/view/activity/ShopActivity.kt
+++ app/src/main/java/com/example/cabifyshop/ui/main/view/activity/ShopActivity.kt
@@ -5,19 +5,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cabifyshop.R
+import com.example.cabifyshop.databinding.ShopActivityBinding
+import com.example.cabifyshop.ui.main.view.fragment.CartFragment
 import com.example.cabifyshop.ui.main.view.fragment.ProductsFragment
 
 class ShopActivity : AppCompatActivity() {
 
+    private lateinit var binding: ShopActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.shop_activity)
+        binding = ShopActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, ProductsFragment.newInstance())
                 .commitNow()
         }
+        setupMenu()
+    }
 
+    private fun setupMenu() {
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.cart -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, CartFragment.newInstance())
+                        .commitNow()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     companion object {
